fb067863708c355554fde68e9bf7558dee559525
JadHajali1 <jadhajali773@gmail.com>
2025-02-20 20:07:52 +0200

    Ask nabiha

---
+++ nabiha.py
@@ -0,0 +1,9 @@
+
+
+monthlySalary = int(input("Enter your monthly salar in $: "))
+monthName = input("Enter the month name: ")
+saving = int(input("Enter your savings percentages: ")) 
+rent = int(input("Enter your rent percentages: ")) 
+electricity = int(input("Enter your electricity percentages: ")) 
+
+
