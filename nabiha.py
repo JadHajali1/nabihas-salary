ce0fa9351af78c76eab7987444aa4f01a582fa01
JadHajali1 <jadhajali773@gmail.com>
2025-02-21 21:18:30 +0200

    Implement while loop for each month information

--- nabiha.py
+++ nabiha.py
@@ -1,28 +1,36 @@
-monthlySalary = int(input("Enter your monthly salar in $: "))
-monthName = input("Enter the month name: ")
-saving = int(input("Enter your savings percentages: "))
-rent = int(input("Enter your rent percentages: ")) 
-electricity = int(input("Enter your electricity percentages: ")) 
 
-savingPer =  (saving / 100) * monthlySalary
-rentPer = (rent / 100) * monthlySalary
-elecPer = (electricity / 100) * monthlySalary
+savingList = []
 
-total = savingPer + rentPer + elecPer
-remainder =  monthlySalary - total
-yearlyRent = rentPer * 12
-yearlyElec = elecPer * 12
-total_salary = remainder ** 2
+answer = "yes"
+while answer != "no":
+    monthlySalary = int(input("Enter your monthly salar in $: "))
+    monthName = input("Enter the month name: ")
+    saving = int(input("Enter your savings percentages: "))
+    rent = int(input("Enter your rent percentages: ")) 
+    electricity = int(input("Enter your electricity percentages: ")) 
 
+    savingPer =  (saving / 100) * monthlySalary
+    rentPer = (rent / 100) * monthlySalary
+    elecPer = (electricity / 100) * monthlySalary
 
-print(f"you have saved in {monthName}: {savingPer}$, and you spend in rents: {rentPer}$, and your electricity is: {elecPer}$")
-print()
-print(f"your total amount: {total}$")
-print()
-print(f"your remainder after expenses: {remainder}$ ")
-print()
-print(f"your yearly rents : {yearlyRent}$ ")
-print()
-print(f"your yearly Electricity : {yearlyElec}$ ")
-print()
-print(f"your remainder salary power 2: {total_salary}$ ")
+    total = savingPer + rentPer + elecPer
+    remainder =  monthlySalary - total
+    yearlyRent = rentPer * 12
+    yearlyElec = elecPer * 12
+    total_salary = remainder ** 2
+
+    savingList.append(savingPer)
+
+    print(f"you have saved in {monthName}: {savingPer}$, and you spend in rents: {rentPer}$, and your electricity is: {elecPer}$")
+    print()
+    print(f"your total amount: {total}$")
+    print()
+    print(f"your remainder after expenses: {remainder}$ ")
+    print()
+    print(f"your yearly rents : {yearlyRent}$ ")
+    print()
+    print(f"your yearly Electricity : {yearlyElec}$ ")
+    print()
+    print(f"your remainder salary power 2: {total_salary}$ ")
+    print()
+    answer = input("do you want to continue?(if you want to stop input NO): ").lower()
