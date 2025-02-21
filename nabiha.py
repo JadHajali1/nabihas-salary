b1c5c25bd5a572d3198ebb978576452d3d674fd4
JadHajali1 <jadhajali773@gmail.com>
2025-02-21 23:50:17 +0200

    Add if statment for percentages

--- nabiha.py
+++ nabiha.py
@@ -6,7 +6,7 @@ while answer != "no":
     monthlySalary = int(input("Enter your monthly salar in $: "))
     monthName = input("Enter the month name: ")
     saving = int(input("Enter your savings percentages: "))
-    rent = int(input("Enter your rent percentages: ")) 
+    rent = int(input("Enter your rent percentages: "))
     electricity = int(input("Enter your electricity percentages: ")) 
 
     savingPer =  (saving / 100) * monthlySalary
