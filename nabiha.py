b3679fad363297d0c13f350b582083f42ecdd7d7
JadHajali1 <jadhajali773@gmail.com>
2025-02-21 20:55:06 +0200

    Add print

--- nabiha.py
+++ nabiha.py
@@ -1,18 +1,28 @@
-
-
 monthlySalary = int(input("Enter your monthly salar in $: "))
 monthName = input("Enter the month name: ")
 saving = int(input("Enter your savings percentages: "))
 rent = int(input("Enter your rent percentages: ")) 
 electricity = int(input("Enter your electricity percentages: ")) 
 
-savingPEr =  (saving / 100) * monthlySalary
+savingPer =  (saving / 100) * monthlySalary
 rentPer = (rent / 100) * monthlySalary
 elecPer = (electricity / 100) * monthlySalary
 
-total = savingPEr + rentPer + elecPer
+total = savingPer + rentPer + elecPer
 remainder =  monthlySalary - total
-yearlyRandE = rentPer + elecPer * 12
+yearlyRent = rentPer * 12
+yearlyElec = elecPer * 12
 total_salary = remainder ** 2
 
-print(f"{total}$, {remainder}$, {yearlyRandE}$, {total_salary}$")
+
+print(f"you have saved in {monthName}: {savingPer}$, and you spend in rents: {rentPer}$, and your electricity is: {elecPer}$")
+print()
+print(f"your total amount: {total}$")
+print()
+print(f"your remainder after expenses: {remainder}$ ")
+print()
+print(f"your yearly rents : {yearlyRent}$ ")
+print()
+print(f"your yearly Electricity : {yearlyElec}$ ")
+print()
+print(f"your remainder salary power 2: {total_salary}$ ")
